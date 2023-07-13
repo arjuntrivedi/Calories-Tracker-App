459cf17d76b4743791a5690afa0f37df0b185754
arjuntrivedi <arjuntrivedi11@gmail.com>
2023-07-13 12:41:51 -0400

    Created App

---
+++ src/components/AppControls/AppControlsInputs.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const AppControlsInputs = ({addMealsHandler, calories, mealName, setCalories, setMealName}) => {
+    const onAddMealsClick = () =>{
+        addMealsHandler();
+    }
+  return (
+    <div className = "app__controls">
+        <div className = "app__controls__inputs">
+            <input type = "text" 
+            placeholder = "Enter Meal" 
+            value = {mealName} 
+            onChange = {(e)=>setMealName(e.target.value)}
+            />
+
+            <input type = "number" 
+            placeholder = "Calories" 
+            value = {calories} 
+            onChange = {(e) => setCalories(e.target.value)}
+            min = {0}
+            />
+            <button onClick = {onAddMealsClick}>Add Meal</button>
+        </div>
+    </div>
+  )
+}
+
+export default AppControlsInputs
